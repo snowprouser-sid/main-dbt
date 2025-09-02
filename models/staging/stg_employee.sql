3380cc4fec44dab4f30321aad7f1b05721bc0b26
Your Name <snowprouser@gmail.com>
2025-09-03 01:14:44 +0530

    add

--- models/staging/stg_employee.sql
+++ models/staging/stg_employee.sql
@@ -13,4 +13,4 @@ from {{ source('fivetran','employee')}}
   -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
 where _FIVETRAN_SYNCED >= (select coalesce(max(_FIVETRAN_SYNCED),'1900-01-01') from {{ this }} )
 
-{% endif %}
+{% endif %} 
